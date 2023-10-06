3de2472e1c7fba0b19c0ed39cd7e1fa9ec2083e3
nguyenluongthang <114991938+nguyenluongthang@users.noreply.github.com>
2023-10-06 18:16:13 +0700

    fix giao dienj

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 import Overview from '@/components/Overview'
+import Location from '@/components/location'
 
 export default function Home() {
   return (
     <div>
       <Overview />
+      <Location />
     </div>
 
   )
