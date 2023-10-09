3516a531e51935b2a9f9d947320a30dd0c4effb7
Thi <nguyenducthi252003@gmail.com>
2023-10-09 15:23:13 +0700

    Update-Components-Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
     <div>
       <div className="w-auto h-auto bg-[#11395B] ">
         <footer className=" dark:bg-gray-900 py-10 ">
-          <div className=" w-auto  grid sm:grid-cols-1 md:grid-cols-[20%,35%,auto] lg:grid-cols-[20%,35%,35%]  gap-[5%]  lg:px-10 py-10">
-            <div className="">
+          <div className=" w-auto grid sm:grid-cols-1 md:grid-cols-[20%,35%,auto] lg:grid-cols-[20%,35%,35%]  gap-[5%]  lg:px-10">
+            <div className="px-1">
               <a href="">
                 {" "}
                 <img
@@ -19,28 +19,28 @@ const Footer = () => {
                 />
               </a>
             </div>
-            <div className="">
-              <h3 className="text-[#FADE83] font-serif ">THÔNG TIN LIÊN HỆ</h3>
+            <div className="px-1">
+              <h3 className="text-[#FADE83] font-serif-[18.2px] font-bold">THÔNG TIN LIÊN HỆ</h3>
               <div className="grid grid-rows gap-2 pt-4  justify-start">
-                <div className="flex text-[#ffffff] mb-2.5 mt-4">
+                <div className="flex text-[#ffffff] mb-2.5 mt-4 items-center">
                   <BsTelephone />
-                  <span className="ml-3 ">0898.186.999</span>
+                  <span className="ml-3 font-sans">0898.186.999</span>
                 </div>
-                <div className="flex text-[#ffffff] mb-2.5">
+                <div className="flex text-[#ffffff] mb-2.5 items-center">
                   <HiOutlineMail />
-                  <span className="ml-3 ">info@wisteria.vn</span>
+                  <span className="ml-3 font-sans">info@wisteria.vn</span>
                 </div>
 
-                <div className="flex text-[#ffffff] mb-2.5">
-                  <IoLocationOutline />
-                  <span className="ml-3 ">
+                <div className="flex text-[#ffffff] mb-2.5 ">
+                  <IoLocationOutline size={25}/>
+                  <span className="ml-3 font-sans">
                     Km14-16,QL32,Xã Kim Chung – Di Trạch,Hoài Đức, Hà Nội
                   </span>
                 </div>
               </div>
               <div>
-                <div className="flex gap-3 pt-3">
-                  <a href="">
+                <div className="flex gap-3 pt-3 items-center">
+                  <a href="#">
                     <img
                       width="48"
                       height="48"
@@ -48,10 +48,10 @@ const Footer = () => {
                       alt="facebook-new"
                     />
                   </a>
-                  <a href="">
+                  <a href="#">
                     <img
-                      width="48"
-                      height="48"
+                      width="44"
+                      height="44"
                       src="https://img.icons8.com/flat-round/64/youtube-play.png"
                       alt="youtube-play"
                     />
@@ -59,19 +59,19 @@ const Footer = () => {
                 </div>
               </div>
             </div>
-            <div className="">
-              <h3 className="text-[#FADE83] font-serif">LƯU Ý</h3>
+            <div className="px-1">
+              <h3 className="text-[#FADE83] font-serif-[18.2px] font-bold">LƯU Ý</h3>
               <div className="grid grid-rows gap-2 pt-4 justify-start">
-                <div className="flex text-[#ffffff] pb-2.5">
-                  <p className="text-[#ffffff] pb-6">
+                <div className="flex text-[#ffffff] md:pb-2.5">
+                  <p className="text-[#ffffff] pb-6 font-sans">
                     Đây là trang thông tin trực tuyến chính thức của dự án The
                     Wisteria – Hinode Royal Park cung cấp tới quý khách hàng các
                     thông tin chính thức về bảng giá, thiết kế và các chính sách
                     bán hàng.
                   </p>
                 </div>
-                <div className="flex items-center">
-                  <p className="text-[#ffffff]">
+                <div className="flex items-center ">
+                  <p className="text-[#ffffff] font-sans pb-12 sm:pb-0">
                     Trực tiếp đặt lịch hẹn & tham quan căn hộ thực tế và trải
                     nghiệm sống hạnh phúc tại The Wisteria.
                   </p>
@@ -81,11 +81,12 @@ const Footer = () => {
           </div>
         </footer>
       </div>
-      <div className="bg-[#102947] w-auto md:h-[36px] py-5 text-center  flex sm:justify-center md:justify-center items-center ">
-        <span className="text-[#ffffff]">
-          Copyright 2023 - The Wisteria. Mọi quyền được bảo lưu!
+      <div className="bg-[#102947] w-auto md:h-[36px]  py-5 text-center  flex sm:justify-center md:justify-center items-center ">
+        <span className="text-[#ffffff] font-sans px-2">
+          Copyright 2023 - <a href="#" className="no-underline">The Wisteria</a> . Mọi quyền được bảo lưu!
         </span>
       </div>
+
     </div>
   );
 };
