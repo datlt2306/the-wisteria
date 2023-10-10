c0bea092f8203165dfa467b12afee9e6a9f84bd1
ngovanvu203 <128632824+ngovanvu2003@users.noreply.github.com>
2023-10-10 09:57:10 +0700

    fix dropdown

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { Fade } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
-import Image from "next/image"
+import React from "react";
+import { Fade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import Image from "next/image";
 
 const Banner = () => {
   const images = [
@@ -11,24 +11,24 @@ const Banner = () => {
     "https://wisteria.vn/wp-content/uploads/2023/08/slide-2.webp",
   ];
   return (
-    <div >
+    <div>
       {/* <Image className='object-fill bg-center' src="https://wisteria.vn/wp-content/uploads/2022/07/Banner-1.webp" fill objectFit="cover" alt="" /> */}
-      <div className='relative z-1'>
+      <div className="">
         <Fade>
-            <div className="each-slide">
+          <div className="each-slide">
             <div>
-                <Image fill objectFit='cover' src={images[0]} alt={''} />
+              <Image fill objectFit="cover" src={images[0]} alt={""} />
             </div>
-            </div>
-            <div className="each-slide">
+          </div>
+          <div className="each-slide">
             <div>
-                <img className='w-[100%]' src={images[1]} alt={''} />
+              <img className="w-[100%]" src={images[1]} alt={""} />
             </div>
-            </div>
+          </div>
         </Fade>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- components/Header/Item.tsx
+++ components/Header/Item.tsx
@@ -1,12 +1,36 @@
 "use client";
+import React, { useState, useEffect } from "react";
 
 import Script from "next/script";
 import { HiSearch } from "react-icons/hi";
 import "flowbite";
 const Items = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  let hoverTimer: string | number | NodeJS.Timeout | undefined;
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleMouseEnter = () => {
+    hoverTimer = setTimeout(() => {
+      setIsDropdownOpen(true);
+    }, 500);
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(hoverTimer);
+    setIsDropdownOpen(false);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(hoverTimer);
+    };
+  }, [hoverTimer]);
   return (
     <>
-      <ul className="flex  flex-col font-medium p-4 tablet:p-0  mt-4 border border-gray-100 rounded-lg bg-gray-50 tablet:flex-row tablet:space-x-8 tablet:mt-0 tablet:border-0 tablet:bg-white dark:bg-gray-800 tablet:dark:bg-[#11395b] dark:border-gray-700">
+      <ul className="flex z flex-col font-medium p-4 tablet:p-0  mt-4 border border-gray-100 rounded-lg bg-gray-50 tablet:flex-row tablet:space-x-8 tablet:mt-0 tablet:border-0 tablet:bg-white dark:bg-gray-800 tablet:dark:bg-[#11395b] dark:border-gray-700">
         <li>
           <a
             href=""
@@ -33,62 +57,70 @@ const Items = () => {
           </a>
         </li>
         <li>
-          <button
-            id="dropdownNavbarLink"
-            data-dropdown-toggle="dropdownNavbar"
-            className="flex text-[13px]  font-[700] items-center justify-between w-full py-2 pl-3 pr-4 text-[#11395b] rounded hover:bg-gray-100 tablet:hover:bg-transparent tablet:border-0 tablet:hover:text-yellow tablet:p-0 tablet:w-auto dark:text-white tablet:dark:hover:text-yellow dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 tablet:dark:hover:bg-transparent"
-          >
-            MẶT BẰNG{" "}
-            <svg
-              className="w-2.5 h-2.5 ml-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
-          {/* Dropdown menu */}
           <div
-            id="dropdownNavbar"
-            className="z-10  font-normal   bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={toggleDropdown}
           >
-            <ul
-              className="py-2 text-sm bg-[rgba(30,70,90,0.85)]  text-white dark:text-gray-400"
-              aria-labelledby="dropdownLargeButton"
+            <button
+              id="dropdownDelayButton"
+              className="flex relative z-30 text-[13px]  font-[700] items-center justify-between w-full py-2 pl-3 pr-4 text-[#11395b] rounded hover:bg-gray-100 tablet:hover:bg-transparent tablet:border-0 tablet:hover:text-yellow tablet:p-0 tablet:w-auto dark:text-white tablet:dark:hover:text-yellow dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 tablet:dark:hover:bg-transparent"
+              type="button"
             >
-              <li>
-                <a
-                  href=""
-                  className="block text-left font-[700]  px-4 py-2 hover:bg-gray-100 hover:text-yellow dark:hover:bg-gray-600 dark:hover:text-white"
+              MẶT BẰNG
+              <svg
+                className="w-2.5 h-2.5 ml-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            {isDropdownOpen && (
+              <div
+                id="dropdownDelay"
+                className=" absolute z-10    block border-0 bg-white divide-y  divide-gray-100  shadow w-44 dark:bg-gray-700"
+              >
+                <ul
+                  className="py-2 text-sm bg-[rgba(30,70,90,0.85)]  text-white dark:text-gray-400"
+                  aria-labelledby="dropdownDelayButton"
                 >
-                  Block A
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  className="block text-left font-[700] px-4 py-2 hover:bg-gray-100 hover:text-yellow dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Block B
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  className="block text-left font-[700] px-4 py-2 hover:bg-gray-100 hover:text-yellow dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Shop House
-                </a>
-              </li>
-            </ul>
+                  <li>
+                    <a
+                      href="#"
+                      className="z-10 block text-left font-[700]  px-4 py-2 hover:bg-gray-100 hover:text-yellow dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Block A
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block text-left font-[700]  px-4 py-2 hover:bg-gray-100 hover:text-yellow dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Block B
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block text-left font-[700]  px-4 py-2 hover:bg-gray-100 hover:text-yellow dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Shop House
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </li>
         <li>
